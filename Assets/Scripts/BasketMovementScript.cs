3c99040684b601e1702d8d85e07bc9cb192115a8
VerifyingUsername <lkyben6@gmail.com>
2022-01-13 11:02:55 +0800

    Collision detection for food

---
+++ Assets/Scripts/BasketMovementScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketMovementScript : MonoBehaviour
+{
+    public float speed;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      float horizontalInput = Input.GetAxis("Horizontal");
+
+      transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+
+        
+       
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Healthy")
+        {
+            print("Add score");
+        }
+
+        if (collision.gameObject.tag == "Unhealthy")
+        {
+            print("dead");
+        }
+    }
+
+
+
+}
